506e22c582ccf0a4a0fd89f55d79976681fbf5dd
PSG-ONE <santagadea.ia@gmail.com>
2025-10-03 22:52:31 -0500

    Arreglado error: wss no estaba definido

--- server.js
+++ server.js
@@ -1,19 +1,40 @@
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// servir client.html
+app.use(express.static(path.join(__dirname)));
+
+// Crear servidor HTTP con Express
+const server = http.createServer(app);
+
+// Crear servidor WebSocket sobre el mismo HTTP
+const wss = new WebSocket.Server({ server });
+
 wss.on("connection", (ws) => {
-  console.log("Nuevo cliente conectado");
+    console.log("Nuevo cliente conectado");
 
-  ws.on("message", (msg) => {
-    try {
-      const data = JSON.parse(msg);
-      wss.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send(JSON.stringify({ user: data.user, msg: data.msg }));
-        }
-      });
-    } catch (err) {
-      console.error("Error procesando mensaje:", err);
-    }
-  });
+    ws.on("message", (msg) => {
+        console.log("Mensaje recibido:", msg.toString());
+        // reenviar mensaje a todos los clientes
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(msg.toString());
+            }
+        });
+    });
 
-  ws.on("close", () => console.log("Cliente desconectado"));
+    ws.on("close", () => {
+        console.log("Cliente desconectado");
+    });
 });
 
+// Escuchar en el puerto que Render asigne
+server.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+});
